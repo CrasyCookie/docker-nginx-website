0ddbeb8f9b73940a08d53be55bcb9b74d669051e
CrasyCookie <mail-forward@mail.crasycookie.com>
2025-03-11 17:33:13 +0100

    Changed the website directory to be modifiable during runtime

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -4,4 +4,9 @@ self="$(dirname "$(realpath "$0")")"
 
 echo "Building..."
 docker buildx build -t nginx-ssi-test "$self" && echo "Deploying..." || exit 1
-docker run -d -p 80:80 --name nginx-webserver nginx-ssi-test && echo "Done." || exit 2
+#docker run -d -p 80:80 --name nginx-webserver nginx-ssi-test && echo "Done." || exit 2
+docker run \
+    --publish 80:80 \
+    --mount type=bind,source="$self/website",target="/srv/www/website",readonly \
+    --name nginx-webserver \
+    --detach nginx-ssi-test && echo "Done." || exit 2
