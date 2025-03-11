ae887404efab93ee5a68bba61196779375bad80d
CrasyCookie <mail-forward@mail.crasycookie.com>
2025-03-11 19:05:41 +0100

    Allow the user to change the container name
    Signed-off-by: CrasyCookie <mail-forward@mail.crasycookie.com>

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 self="$(dirname "$(realpath "$0")")"
+if [ -n "$1" ]; then
+    name="$1"
+else
+    name="website"
+fi
 
 echo "Building..."
 docker buildx build -t nginx-ssi-test "$self" && echo "Deploying..." || exit 1
@@ -8,5 +13,5 @@ docker buildx build -t nginx-ssi-test "$self" && echo "Deploying..." || exit 1
 docker run \
     --publish 80:80 \
     --mount type=bind,source="$self/website",target="/srv/www/website",readonly \
-    --name nginx-webserver \
+    --name "$name" \
     --detach nginx-ssi-test && echo "Done." || exit 2
