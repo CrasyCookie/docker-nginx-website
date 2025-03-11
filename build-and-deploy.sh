af81b36bc0524ceff09a124fb0c2169cf2b3080d
CrasyCookie <mail-forward@mail.crasycookie.com>
2025-03-11 19:10:34 +0100

    Remove commented out code
    Signed-off-by: CrasyCookie <mail-forward@mail.crasycookie.com>

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -9,7 +9,6 @@ fi
 
 echo "Building..."
 docker buildx build -t nginx-ssi-test "$self" && echo "Deploying..." || exit 1
-#docker run -d -p 80:80 --name nginx-webserver nginx-ssi-test && echo "Done." || exit 2
 docker run \
     --publish 80:80 \
     --mount type=bind,source="$self/website",target="/srv/www/website",readonly \
